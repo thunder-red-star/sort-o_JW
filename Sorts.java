64d83d79fc844f0a61c188aa1c4fb50a3eebc9e9
William <67127399+thunder-red-star@users.noreply.github.com>
2022-01-06 12:28:49 -0500

    JABA

---
+++ Sorts.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Sorts {
+    public static void insertionSortV( ArrayList<Comparable> data )
+    {
+        int partition = 0;
+        for( int i = 1; i < data.size(); i++ ) {
+            partition = i;
+            for( int j = i; j > 0; j-- ) {
+                if ( data.get( j ).compareTo( data.get( j-1 ) ) < 0 )
+                    data.set( j, data.set( j-1, data.get( j ) ) );
+                else
+                    break;
+            }
+        }
+    }
+
+    public static ArrayList<Comparable> insertionSort( ArrayList<Comparable> input )
+    {
+        ArrayList<Comparable> data = new ArrayList<Comparable>();
+        for( Comparable o : input )
+            data.add( o );
+        insertionSortV( data );
+        return data;
+    }
+
+    public static void selectionSortV(ArrayList<Comparable> a) {
+        int minIndex;
+        for (int i = 0; i < a.size() - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < a.size(); j++) {
+                if (a.get(j).compareTo(a.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+            Comparable temp = a.get(i);
+            a.set(i, a.get(minIndex));
+            a.set(minIndex, temp);
+        }
+    }
+
+    public static ArrayList<Comparable> selectionSort( ArrayList<Comparable> input )
+    {
+        ArrayList<Comparable> data = new ArrayList<Comparable>();
+        for( Comparable o : input )
+            data.add( o );
+        selectionSortV( data );
+        return data;
+    }
+
+    public static ArrayList<Comparable> bubbleSort ( ArrayList<Comparable> data ) {
+        for( int i = 0; i < data.size(); i++ ) {
+            for( int j = 0; j < data.size()-1-i; j++ ) {
+                if( data.get(j).compareTo(data.get(j+1)) > 0 ) {
+                    Comparable temp = data.get(j);
+                    data.set(j, data.get(j+1));
+                    data.set(j+1, temp);
+                }
+            }
+        }
+        return data;
+    }
+}
