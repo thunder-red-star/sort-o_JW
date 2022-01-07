f86ab6c0b49384607a96b08bc77062c6f3c2d65f
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2022-01-07 00:02:49 -0500

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
 
 public class Sorts {
+
     public static void insertionSortV( ArrayList<Comparable> data )
     {
         int partition = 0;
+        int insertionSwap = 0;
         for( int i = 1; i < data.size(); i++ ) {
             partition = i;
             for( int j = i; j > 0; j-- ) {
                 if ( data.get( j ).compareTo( data.get( j-1 ) ) < 0 )
+                    {
                     data.set( j, data.set( j-1, data.get( j ) ) );
+                    insertionSwap += 1;
+                  }
                 else
                     break;
             }
         }
+        System.out.println("Total Swaps: " + insertionSwap);
     }
 
     public static ArrayList<Comparable> insertionSort( ArrayList<Comparable> input )
@@ -26,6 +32,7 @@ public class Sorts {
 
     public static void selectionSortV(ArrayList<Comparable> a) {
         int minIndex;
+        int selectionSwap = 0;
         for (int i = 0; i < a.size() - 1; i++) {
             minIndex = i;
             for (int j = i + 1; j < a.size(); j++) {
@@ -36,7 +43,9 @@ public class Sorts {
             Comparable temp = a.get(i);
             a.set(i, a.get(minIndex));
             a.set(minIndex, temp);
+            selectionSwap += 1;
         }
+        System.out.println("Total Swaps: " + selectionSwap);
     }
 
     public static ArrayList<Comparable> selectionSort( ArrayList<Comparable> input )
@@ -49,15 +58,18 @@ public class Sorts {
     }
 
     public static ArrayList<Comparable> bubbleSort ( ArrayList<Comparable> data ) {
+        int bubbleSwap = 0;
         for( int i = 0; i < data.size(); i++ ) {
             for( int j = 0; j < data.size()-1-i; j++ ) {
                 if( data.get(j).compareTo(data.get(j+1)) > 0 ) {
                     Comparable temp = data.get(j);
                     data.set(j, data.get(j+1));
                     data.set(j+1, temp);
+                    bubbleSwap += 1;
                 }
             }
         }
+        System.out.println("Total Swaps: " + bubbleSwap);
         return data;
     }
 }
